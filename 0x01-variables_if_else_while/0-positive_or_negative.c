4906c084353fa13f1feddfac50074daf155b1b9c
ADeEFA <dheby.alabi@gmail.com>
2022-10-17 10:59:49 -0700

    retrying task1 compilation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,13 +5,12 @@
 #include <time.h>
 
 /**
-
+ *
  * main - entry point
-
+ *
  * Description: prints  if a number is positive or negative.
-
+ *
  * Return: 0
-
  */
 
 int main(void)
@@ -19,9 +18,6 @@ int main(void)
 {
 
     int n;
-
-
-
     srand(time(0));
 
     n = rand() - RAND_MAX / 2;
