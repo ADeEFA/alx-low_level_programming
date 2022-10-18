e803faa8e86795513a3749e856c620e0e44cc90d
ADeEFA <dheby.alabi@gmail.com>
2022-10-18 09:29:35 -0700

    fixing task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,33 +3,30 @@
 #include <unistd.h>
 
 /**
- *
  *  * main - Entry point
  *
  *   * Description: prints char below 10
  *
  *    * Return: Always 0 (success)
- *
- *     */
+ */
 
 int main(void)
+{
+
+int a;
+
+for 
+(a = 0;
+a < 10;
+a++)
 
 {
 
-	    int a;
+putchar(a + '0');
 
+}
 
+putchar('\n');
 
-	        for (a = 0; a < 10; a++)
-
-			    {
-
-				            putchar(a + '0');
-
-					        }
-
-		    putchar('\n');
-
-		        return (0);
-
+return (0);
 }
