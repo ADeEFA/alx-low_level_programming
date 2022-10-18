77c910428c77d37462b45f3ec1d1dd0720eb2f17
ADeEFA <dheby.alabi@gmail.com>
2022-10-18 10:04:28 -0700

    task 100 debugging

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,59 +3,57 @@
 #include <unistd.h>
 
 /**
- *
- *  * main - Entyr point
+ *  * main - Entry point
  *
  *   * Description: prints two digits combination
  *
  *    * Return: Always 0 (success)
- *
- *     */
+ */
 
 int main(void)
 
 {
 
-	    int c, i;
+int c, i;
 
+for
+(c = '0';
+c <= '9';
+c++)
+{
 
+for
+(i = '0';
+i <= '9';
+i++)
+{
 
-	        for (c = '0'; c <= '9'; c++)
+if
+(c < i)
 
-			    {
+{
 
-				            for (i = '0'; i <= '9'; i++)
+putchar(c);
 
-						            {
+putchar(i);
 
-								                if (c < i)
+if
+(c != '8' || (c == '8' && i != '9'))
+{
 
-											            {
+putchar(',');
 
-													                    putchar(c);
+putchar(' ');
 
-															                    putchar(i);
+}
 
+}
 
+}
+}
 
-																	                    if (c != '8' || (c == '8' && i != '9'))
+putchar('\n');
 
-																				                    {
-
-																							                        putchar(',');
-
-																										                    putchar(' ');
-
-																												                    }
-
-																			                }
-
-										        }
-
-					        }
-
-		    putchar('\n');
-
-		        return (0);
+return (0);
 
 }
