21fcf7f556a935fc0fde54d317087ba53ac0d234
ADeEFA <dheby.alabi@gmail.com>
2022-10-21 14:06:03 -0700

    tired

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (c = 48; c <= 57; c++)
 	{
-		for (i = 48; i <=57; i++)
+		for (i = 48; i <= 57; i++)
 		{
 			for (k = 48; k <= 57; k++)
 			{
@@ -27,7 +27,7 @@ int main(void)
 
 				if (c + i + k + j == 227 && c == 57)
 					{ 
-					break;
+						break;
 					}
 					else
 					{
